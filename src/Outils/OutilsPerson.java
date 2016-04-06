e3356514cd93c2b4e880ed7cf700504467df3937
TMichaudel <thibaud.michaudel@gmail.com>
2016-04-06 11:53:08 +0200

    Commit5

---
+++ src/Outils/OutilsPerson.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Outils;
+
+import java.util.GregorianCalendar;
+import java.util.Iterator;
+import java.util.List;
+import persons.IPerson;
+
+/**
+ *
+ * @author Thibaud
+ */
+public class OutilsPerson {
+    
+    public int searchOldest(List<IPerson> lp, GregorianCalendar date) {
+        int max=0;
+        Iterator<IPerson> listIter = lp.iterator();
+		while (listIter.hasNext()) {
+			IPerson pers=listIter.next();
+                        max=Math.max(max, pers.getAge(date));
+		}
+        return max;
+    }
+}
