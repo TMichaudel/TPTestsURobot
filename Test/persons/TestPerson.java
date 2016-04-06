332f810961ba8022f0cfda917fe4e282c2d252b0
TMichaudel <thibaud.michaudel@gmail.com>
2016-04-06 09:33:04 +0200

    Commit3

--- Test/persons/TestPerson.java
+++ Test/persons/TestPerson.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package persons;
 
 import java.util.Date;
@@ -20,24 +19,26 @@ import org.junit.Test;
  * @author Thibaud
  */
 public class TestPerson {
+
     Person person;
+
     public TestPerson() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
-        
+
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
-        person = new Person("toto","tutu",1990,10,12);
+        person = new Person("toto", "tutu", 1990, 10, 12);
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -47,17 +48,23 @@ public class TestPerson {
     //
     // @Test
     // public void hello() {}
-    
-    @Test(expected=IllegalArgumentException.class)
-    public void TestGetAgeException() {
-        
-        GregorianCalendar date0;
+    @Test(expected = IllegalArgumentException.class)
+    public void TestGetAgeFuture() {
+        GregorianCalendar date0 = new GregorianCalendar(1985, 10, 12);
         person.getAge(date0);
+    }
+
+    @Test
+    public void TestGetAge() {
+        GregorianCalendar date0 = new GregorianCalendar(1995, 10, 12);
+        assertEquals(person.getAge(date0), 5);
     }
     
     @Test
-    public void TestGetAge(){
-        GregorianCalendar date0 = new GregorianCalendar(1995,10,12);
-        
-    
+    public void TestWasBorn() {
+        GregorianCalendar date0 = new GregorianCalendar(1985, 10, 12);
+        GregorianCalendar date1 = new GregorianCalendar(1995, 10, 12);
+        assertEquals(person.wasBorn(date0),false);
+        assertEquals(person.wasBorn(date1),true);
+    }
 }
