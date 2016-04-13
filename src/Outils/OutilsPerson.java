55d2df799e58c334e6c39660ed3a25e0d60779e7
MathieuThouret <mathieu.thouret@gmail.com>
2016-04-13 09:29:40 +0200

    Merge origin/master
    Conflicts:
    	src/pop3/Email.java

--- src/Outils/OutilsPerson.java
+++ src/Outils/OutilsPerson.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Outils;
 
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
 import java.util.Iterator;
 import java.util.List;
@@ -16,19 +16,30 @@ import persons.IPerson;
  * @author Thibaud
  */
 public class OutilsPerson {
-    
+
     public int searchOldest(List<IPerson> lp, GregorianCalendar date) {
-        int max=0;
+        int max = 0;
         Iterator<IPerson> listIter = lp.iterator();
-		while (listIter.hasNext()) {
-			IPerson pers=listIter.next();
-                        try {
-                        max=Math.max(max, pers.getAge(date));
-                        }
-                        catch (Exception e ) {
-                            
-                        }
-		}
+        while (listIter.hasNext()) {
+            IPerson pers = listIter.next();
+            try {
+                max = Math.max(max, pers.getAge(date));
+            } catch (Exception e) {
+
+            }
+        }
         return max;
     }
+
+    public List<IPerson> getPeopleInterval(List<IPerson> people, GregorianCalendar date, int ageMin, int ageMax) {
+        List<IPerson> validPeople = new ArrayList<>();
+        for (IPerson person : people) {
+            int age = person.getAge(date);
+            if (age > ageMin && age < ageMax) {
+                validPeople.add(person);
+            }
+        }
+        return validPeople;
+    }
+
 }
